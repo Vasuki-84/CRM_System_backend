f8157fd9c202f0920154e4de405796fe3f4c83f0
Vasuki t <iamvasukithangaraj@gmail.com>
2025-12-23 18:59:43 +0530

    imported CORS for network security

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const dbConnection = require("./config/dbConnection.config");
 const userRoute = require("./routes/user.route");
 const customerRoute = require("./routes/customer.route");
 const dealRoute = require("./routes/deals.route");
+const CORS = require("cors");
 
 dbConnection();
 app.use(express.json());
+app.use(CORS());
 app.use("/user", userRoute);
 app.use("/customer", customerRoute);
-app.use("/deals",dealRoute);
+app.use("/deals", dealRoute);
 app.listen(process.env.port, () => {
   console.log("Server running on Port", process.env.port);
 });
